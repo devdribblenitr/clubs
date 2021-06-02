fc83acb4b31a9d62f0ded9258964236f2a9fdd87
Mohammad Sahil <sahilmejakhas@gmail.com>
2021-06-02 20:37:21 +0530

    modified

--- clubs/src/script.js
+++ clubs/src/script.js
@@ -1,84 +1,100 @@
-// Counter Section
-function inVisible(element) {
-    var WindowTop = $(window).scrollTop();
-    var WindowBottom = WindowTop + $(window).height();
-    var ElementTop = element.offset().top;
-    var ElementBottom = ElementTop + element.height();
-    if ((ElementBottom <= WindowBottom) && ElementTop >= WindowTop)
-      animate(element);
-   }
-   
-   function animate(element) {
-    if (!element.hasClass('ms-animated')) {
-      var maxval = element.data('max');
-      var html = element.html();
-      element.addClass("ms-animated");
-      $({
-        countNum: element.html()
-      }).animate({
-        countNum: maxval
-      }, {
-        duration: 2000,
-        easing: 'linear',
-        step: function() {
-          element.html(Math.floor(this.countNum) + html);
-        },
-        complete: function() {
-          element.html(this.countNum + html);
-        }
+(function ($) {
+	$.fn.countTo = function (options) {
+		options = options || {};
+
+		return $(this).each(function () {
+			// set options for current element
+			var settings = $.extend({}, $.fn.countTo.defaults, {
+				from:            $(this).data('from'),
+				to:              $(this).data('to'),
+				speed:           $(this).data('speed'),
+				refreshInterval: $(this).data('refresh-interval'),
+				decimals:        $(this).data('decimals')
+			}, options);
+
+			// how many times to update the value, and how much to increment the value on each update
+			var loops = Math.ceil(settings.speed / settings.refreshInterval),
+				increment = (settings.to - settings.from) / loops;
+
+			// references & variables that will change with each update
+			var self = this,
+				$self = $(this),
+				loopCount = 0,
+				value = settings.from,
+				data = $self.data('countTo') || {};
+
+			$self.data('countTo', data);
+
+			// if an existing interval can be found, clear it first
+			if (data.interval) {
+				clearInterval(data.interval);
+			}
+			data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+			// initialize the element with the starting value
+			render(value);
+
+			function updateTimer() {
+				value += increment;
+				loopCount++;
+
+				render(value);
+
+				if (typeof(settings.onUpdate) == 'function') {
+					settings.onUpdate.call(self, value);
+				}
+
+				if (loopCount >= loops) {
+					// remove the interval
+					$self.removeData('countTo');
+					clearInterval(data.interval);
+					value = settings.to;
+
+					if (typeof(settings.onComplete) == 'function') {
+						settings.onComplete.call(self, value);
+					}
+				}
+			}
+
+			function render(value) {
+				var formattedValue = settings.formatter.call(self, value, settings);
+				$self.text(formattedValue);
+			}
+		});
+	};
+
+	$.fn.countTo.defaults = {
+		from: 0,               // the number the element should start at
+		to: 0,                 // the number the element should end at
+		speed: 1000,           // how long it should take to count between the target numbers
+		refreshInterval: 100,  // how often the element should be updated
+		decimals: 0,           // the number of decimal places to show
+		formatter: formatter,  // handler for formatting the value before rendering
+		onUpdate: null,        // callback method for every time the element is updated
+		onComplete: null       // callback method for when the element finishes updating
+	};
+
+	function formatter(value, settings) {
+		return value.toFixed(settings.decimals);
+	}
+}(jQuery));
+
+    jQuery(function ($) {
+     
+      // start all the timers
+      $('.timer').each(count);
+      
+      // restart a timer when a button is clicked
+      $( window ).scroll(function () {console.log($(window).scrollTop());
+    if($(window).scrollTop() > 300 && $(window).scrollTop() < 850)
+    {
+       $('.timer').each(count);
+     }
       });
-    }
-   
-   }
-   
-   $(function() {
-    $(window).scroll(function() {
-      $("h2[data-max]").each(function() {
-        inVisible($(this));
-      });
-    })
-   });
-
-
-
-
-
-
-
-
-
-
-
-
-var counted = 0;
-$(window).scroll(function() {
-
-  var oTop = $('#counter').offset().top - window.innerHeight;
-  if (counted == 0 && $(window).scrollTop() > oTop) {
-    $('.count').each(function() {
-      var $this = $(this),
-        countTo = $this.attr('data-count');
-      $({
-        countNum: $this.text()
-      }).animate({
-          countNum: countTo
-        },
-
-        {
-
-          duration: 2000,
-          easing: 'swing',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $this.text(this.countNum);
-            //alert('finished');
-          }
-
-        });
+      
+      function count(options) {
+        var $this = $(this);
+        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+        $this.countTo(options);
+      }
     });
-    counted = 1;
-  }
-
-});
